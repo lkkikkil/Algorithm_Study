82c197e5d392ca3b4bd4f16796896b5c4046bce7
lkkikkil <skattr13@naver.com>
2023-08-28 23:42:25 +0900

    백준 15652번 문제 해결

---
+++ brute_force/15652.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int myStack[10];
+
+void myPrint(int startNum, int length) {
+	if (length == m) {
+		for (int idx = 0; idx < m; idx++) {
+			cout << myStack[idx] << " ";
+		}
+		cout << "\n";
+	}
+	else {
+		for (int num = startNum; num <= n; num++) {
+			myStack[length] = num;
+			myPrint(num, length + 1);
+		}
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	myPrint(1, 0);
+
+	return 0;
+}
