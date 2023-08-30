aba27a63c11766c729735ca5a328aebd1e05edae
lkkikkil <skattr13@naver.com>
2023-08-30 14:49:45 +0900

    백준 15657번 문제 해결

---
+++ brute_force/15657.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int num[10001];
+int myStack[10];
+
+void myPrint(int startIndex, int length) {
+	if (length == m) {
+		for (int idx = 0; idx < m; idx++) {
+			cout << myStack[idx] << " ";
+		}
+		cout << "\n";
+	}
+	else {
+		for (int numIndex = startIndex; numIndex < n; numIndex++) {
+			myStack[length] = num[numIndex];
+			myPrint(numIndex, length + 1);
+		}
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	for (int numCount = 0; numCount < n; numCount++) {
+		cin >> num[numCount];
+	}
+	sort(num, num + n);
+
+	myPrint(0, 0);
+
+	return 0;
+}
